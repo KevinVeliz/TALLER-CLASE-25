93a92f0c506675511dea39080efc522a9fc86238
Kevin Veliz <mario-veliz258934@hotmail.com>
2020-08-25 21:02:21 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+const int N = 100;
+
+int main() 
+{
+
+   int a[N];
+   int aux,n,i,AUX1;
+   char r;
+  ofstream datos;
+  string nombreArchivo;
+  cout<<"INGRESA EL NOMBRE DEL ARCHIVO: ";
+  cin.ignore();
+  getline(cin,nombreArchivo);
+  datos.open(nombreArchivo.c_str(),ios::app);
+do
+  {
+  cout<<"Ingresa el tamaño del arreglo: ";
+  cin>>n;
+  datos<<"El arreglo original es: ";
+  for(int i=0;i<n;i++)
+  {
+    cout<<"Ingresa un numero positivo de la posición ["<<i<<"] : ";
+    cin>>a[i];
+    AUX1=AUX1+a[i];
+    datos<<"["<<a[i]<<"]";
+  }
+
+  cout<<"La suma es: "<<AUX1<<endl;
+  datos<<"\nLa suma es: "<<AUX1<<endl;
+  for (int i=0; i<n; i++)
+    {  
+
+        aux=a[i];
+        int p=i;
+        while (p>0)
+        {
+           if (a[p-1]>aux)
+            {
+                a[p]=a[p-1];
+                a[p-1]=aux;
+            }
+            p--;
+        }
+        
+    }
+    cout<<"Los valores ordenados son: ";
+    datos<<"Los valores ordenados son: ";
+    for (int i=0; i<n; i++)
+    {
+      cout<<"["<<a[i]<<"]";
+      datos<<"["<<a[i]<<"]";
+    }
+  datos<<endl;
+  datos<<"Los elementos totales son: "<<n;
+  datos<<endl;
+  cout<<"\nDESEA AGREGAR OTRO DATO s/n: ";
+  cin>>r;
+  cin.ignore();
+  }while(r=='s');
+  datos.close();
+    return 0;
+}
